3af215a90ca75d2a32bb40a4cdfc964793b31e27
j1wu <ji.wu@outlook.com>
2017-03-29 23:17:43 +0900

    Add webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist',
+    filename: 'bundle.js'
+  }
+};
