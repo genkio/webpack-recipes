624c751cfa137a7f5764ffdea8bc1777f30dcabb
j1wu <ji.wu@outlook.com>
2017-03-30 10:38:34 +0900

    Enable webpack hot module replacement

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ var messages = require('./messages');
 
 var app = document.getElementById('app');
 app.innerHTML = '<p>Hello ' + messages.hi + '!</p>';
+
+if (module.hot) {
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    './src/index.js',
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080'
+  ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/dist',
+    publicPath: '/dist/',
     filename: 'bundle.js'
   }
 };
